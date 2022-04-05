c8f6263b98a640435ef3db79bf84768cf0a715cd
Jorge Guerrero <jorge.andres.gs2001@gmail.com>
2022-04-04 21:57:35 -0300

    api fix

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '2b4f72e9-8101-4e6b-8c5a-d3ecb3683583';
 export const MODAL_CLOSE_SEC = 2.5;
